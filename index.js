2ea6c7628478f360793bfe492409ff10ab740560
x5bgray <x5bgray@gmail.com>
2023-06-26 23:02:10 -0500

    added something

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var https = require('https');
 var fs = require('fs');
 
+
+var log_stream = fs.createWriteStream('./log/node.log');
+process.stdout.write = process.stderr.write = log_stream.write.bind(log_stream);
+
+process.on('uncaughtException', function(err) {
+  console.error((err && err.stack) ? err.stack : err);
+});
+
+
 var options = {
   key: fs.readFileSync('ssl/pub'),
   cert: fs.readFileSync('ssl/cert')
